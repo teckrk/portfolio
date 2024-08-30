48cd067403b23d7686d1f04c6f9da6a2aa94d2dd
teckrk <rohanmehra9149474868@gmail.com>
2024-08-30 18:44:41 +0530

    new commit ---

--- script.js
+++ script.js
@@ -47,3 +47,29 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
+
+// function emailSend() {
+// var username = document.getElementById('name').value;
+// var phone = document.getElementById('mobile').value;
+// var email = document.getElementById('email').value;
+// var subject = document.getElementById('subject').value;
+// var textarea = document.getElementById('textarea').value;
+
+// var messagebody = "Name " + username +
+// "<br/> Phone" + phone +
+// "<br/> Email" + email +
+// "<br/> Subject" + subject +
+// "<br/> Message" + textarea ; 
+
+//     Email.send({
+//         Host : "smtp.elasticemail.com",
+//         Username : "doremonkumar07@gmail.com",
+//         Password : "370DCC9ACDEA0917EC3C5A33F0D34D12086E",
+//         To : 'doremonkumar07@gmail.com',
+//         From :  'doremonkumar07@gmail.com',  
+//         Subject : "Message from Portfolio",
+//         Body : messagebody
+//     }).then(
+//       message => alert(message)
+//     );
+// }
